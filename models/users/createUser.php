c562a16c276232f2ee69032b63d649142121a3b8
Yanis Sabbari <yanis.sabbari54230@live.fr>
2021-07-26 16:06:27 +0200

    Changements du .gitignore, du createUser.php et modifications non indexés du dbConnect.php

--- models/users/createUser.php
+++ models/users/createUser.php
@@ -1,2 +1,14 @@
 <?php
 
+$db = require("./dbConnect.php");
+function createUser(){
+    $db = dbConnect();
+    $UtiPseudo = 'Uti_Pseudo';
+    $UtiLogin = 'Uti_Login';
+    $UtiMdp = 'Uti_Mdp';
+    $UtiDroit = 'Uti_Droit';
+    $user = $db->prepare("INSERT INTO utilisateurs `Uti_Pseudo`, `Uti_Login`, `Uti_Mdp`, `Uti_Droit` VALUES (?, ?, ?, ?)");
+    $lignes_a_affecter = $user->execute(array($UtiPseudo, $UtiLogin, $UtiMdp, $UtiDroit));
+    return $lignes_a_affecter;
+}
+
