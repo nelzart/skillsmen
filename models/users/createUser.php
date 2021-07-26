750797c7eda52c20277239208cb5824f36cb0436
Dorian Bleynie <dorian.bleynie@gmail.com>
2021-07-26 16:46:32 +0200

    mep usersmodels

--- models/users/createUser.php
+++ models/users/createUser.php
@@ -1,14 +1,21 @@
 <?php
 
 $db = require("./dbConnect.php");
-function createUser(){
+
+function createUser()
+{
     $db = dbConnect();
+
     $UtiPseudo = 'Uti_Pseudo';
     $UtiLogin = 'Uti_Login';
     $UtiMdp = 'Uti_Mdp';
     $UtiDroit = 'Uti_Droit';
+
     $user = $db->prepare("INSERT INTO utilisateurs `Uti_Pseudo`, `Uti_Login`, `Uti_Mdp`, `Uti_Droit` VALUES (?, ?, ?, ?)");
+
     $lignes_a_affecter = $user->execute(array($UtiPseudo, $UtiLogin, $UtiMdp, $UtiDroit));
+
     return $lignes_a_affecter;
+    
 }
 
