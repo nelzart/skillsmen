4c6b700edf4ac86471cac3e6d32bf7d70a19b53e
Yanis Sabbari <yanis.sabbari54230@live.fr>
2021-07-26 14:47:56 +0200

    Changements dbConnect.php

--- models/dbConnect.php
+++ models/dbConnect.php
@@ -1,19 +1,4 @@
 <?php
-<<<<<<< HEAD
-    
-    function dbConnect()
-    {
-            $hostname = 'localhost:3306';
-            $dbname = 'skillsmen';
-            $dbuser = 'root';
-            $password = 'YanisPRAYTED54!';
-    
-            $db = new PDO('mysql:host='.$hostname.';dbname='.$dbname.';charset=utf8', $dbuser, $password);
-            return $db;
-    }
-?>
-=======
-
 function dbConnect()
 {
     $hostname = 'localhost';
@@ -23,5 +8,4 @@ function dbConnect()
 
     $db = new PDO('mysql:host='.$hostname.';dbname='.$dbname.';charset=utf8', $dbuser, $password);
     return $db;
-}   
->>>>>>> 89824c41b4c0083e1b32cee9d0b5f00d32f14477
+}
