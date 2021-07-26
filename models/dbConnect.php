3041f27e7698baebc852a1cbb4fc724d59afa110
DavidFred34 <frederic.david@cariforefoccitanie.fr>
2021-07-26 12:34:23 +0200

    modif

--- models/dbConnect.php
+++ models/dbConnect.php
@@ -3,8 +3,8 @@
     function dbConnect()
     {
             $hostname = 'localhost';
-            $dbname = 'test';
-            $dbuser = 'dbuser';
+            $dbname = 'skillsman';
+            $dbuser = 'root';
             $password = '';
     
             $db = new PDO('mysql:host='.$hostname.';dbname='.$dbname.';charset=utf8', $dbuser, $password);
