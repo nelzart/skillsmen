7c21beb4f2ca6534a6cc12b839356e0c89172faf
Yanis Sabbari <yanis.sabbari54230@live.fr>
2021-07-26 14:41:12 +0200

    Changements .gitignore et dbConnect.php

--- models/dbConnect.php
+++ models/dbConnect.php
@@ -2,11 +2,12 @@
     
     function dbConnect()
     {
-            $hostname = 'localhost';
-            $dbname = 'test';
-            $dbuser = 'dbuser';
-            $password = '';
+            $hostname = 'localhost:3306';
+            $dbname = 'skillsmen';
+            $dbuser = 'root';
+            $password = 'YanisPRAYTED54!';
     
             $db = new PDO('mysql:host='.$hostname.';dbname='.$dbname.';charset=utf8', $dbuser, $password);
             return $db;
     }
+?>
