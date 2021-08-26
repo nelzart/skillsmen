66801102f6018fdba132f451d6b35c2e2bff8bc2
DavidFred34 <frederic.david@cariforefoccitanie.fr>
2021-08-26 03:19:54 +0200

    maj sql

---
+++ BDD/alimentationTableTypeCocktailEtTypeIngredientl.sql
@@ -0,0 +1,25 @@
+INSERT INTO `ingredients` (`Ing_Nom`, `Ing_Categorie`) VALUES
+('Rhum', 'base'),
+('vodka', 'base'),
+('coca', 'base'),
+('gin', 'base'),
+('tequila', 'base'),
+('whiskey', 'base'),
+('cognac', 'base'),
+('liqueur', 'base'),
+('champagne', 'base'),
+('bierre', 'base'),
+('vin', 'base');
+
+INSERT INTO `typecocktail` (`Type_Libelle`) VALUES
+('tequila'),
+('whiskey'),
+('gin'),
+('vodka'),
+('cognac'),
+('liqueur'),
+('rhum'),
+('champagne'),
+('sans alcool '),
+('biere'),
+('vin');
