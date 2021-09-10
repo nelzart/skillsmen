17962b56372fd3722776a818dd427d62844f267a
f.david <frederic.david@cariforefoccitanie.fr>
2021-09-10 14:27:27 +0200

    sql de remise à zero base pour enlever les tests

---
+++ BDD/resetBdd.sql
@@ -0,0 +1,7 @@
+DELETE FROM categoriecocktail
+DELETE FROM commentaires
+DELETE FROM likes
+DELETE FROM compositioncocktail
+DELETE FROM images
+DELETE FROM ingredients where Ing_Id>417
+DELETE FROM cocktail
