e5768f1fd2e2bacb08fdb2d46100ac2f794cd45f
Dorian Bleynie <dorian.bleynie@gmail.com>
2021-09-21 23:21:52 +0200

    Bonne chance

--- views/cocktailView.php
+++ views/cocktailView.php
@@ -36,16 +36,16 @@ $title =  $coc[1]  ;
                                 </a> 
                             ';
                 } else {
-                    echo '';
-                }
-                    
-                    ?>
-                    <label name="favorite" class="iconCover">
+                    echo '<label name="favorite" class="iconCover">
                 <input type="checkbox" name="favorite" value="favorite" style="display:none;">
                     <div class="favorite-checkbox">
                         <svg id="favorite" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>
                     </div> 
-                </label>
+                </label>';
+                }
+                    
+                    ?>
+                    
                 
                 
 				<div class="iconCover print" style="margin-left:20px;" height="15%" width="15%">
